f3de90e18dd969b6393320485db8748b6babe8d1
Jeff <jeffersoncerqueira75@gmail.com>
2022-06-03 20:53:20 -0300

    finalizando inicio projeto backend

--- app/Http/Controllers/AdocaoController.php
+++ app/Http/Controllers/AdocaoController.php
@@ -3,10 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Adocao;
 
 class AdocaoController extends Controller
 {
-    public function index(){
-        return "controller das adoções";
+    public function index(Request $request){
+
+        $request->validate([
+            "e-mail" => ["required", "email"],
+            "valor" => ["required", "numeric", "between:10, 100"],
+            "pet_id" => ["required", "int", "exists:pets,id"]
+        ]);
+
+        $dadosDaAdocao = $request->all();
+
+        Adocao::create($dadosDaAdocao);
+        
+        return $dadosDaAdocao;
     }
 }
