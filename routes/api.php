650c6c9f19b4d54c664a7bc94ca1bf786b568211
Jefferson <jeffersoncerqueira75@gmail.com>
2022-06-03 17:39:00 -0300

    migração de adoção

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PetController;
+use App\Http\Controllers\AdocaoController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +19,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/pets', [PetController:: class, 'index']);
+
+Route::post("/adocoes", [AdocaoController:: class, "index"]);
